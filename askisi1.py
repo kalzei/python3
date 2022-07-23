d22f35e9cd1f786ada0cdfb3e0dfea049a93a8ea
kalzei <kzeinaki@gmail.com>
2022-07-23 08:52:53 +0300

    askisi1.py

--- askisi1.py
+++ askisi1.py
@@ -1,8 +1,8 @@
-x=input'dose akeraio bathmous celsious:'
-y=input'dose akeraio arithmo2:'
+x=input('dose akeraio1:')
+y=input('dose akeraio arithmo2:')
 x=int(x)
 y=int(y)
 s=x+y
 m=x*y
-print('\n'sum:{}\n'product:{}.format(s,m))
+print('\nsum:{}\nproduct:{}'.format(s,m))
 print('telos kodika')
