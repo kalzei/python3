dd38679ef24dd6074798317128f3cb66b829a44e
kalzei <kzeinaki@gmail.com>
2022-10-09 20:13:07 +0300

    changed in english

--- week1/askisi1.py
+++ week1/askisi1.py
@@ -1,8 +1,9 @@
-x=input('dose akeraio1:')
-y=input('dose akeraio arithmo2:')
+#sum and product of 2 numbers
+x=input('give num1:')
+y=input('give num2:')
 x=int(x)
 y=int(y)
 s=x+y
 m=x*y
 print('\nsum:{}\nproduct:{}'.format(s,m))
-print('telos kodika')
+print('the end')
