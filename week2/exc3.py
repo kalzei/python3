b5fb1d5e72830c067f150556eae82dad9535d8cd
Kalliopizei <k.zeinaki@digital-science.com>
2022-11-08 08:48:31 +0200

    exc.3.new

--- week2/exc3.py
+++ week2/exc3.py
@@ -1,3 +1,5 @@
+#asks for income, adds rate and give back in how many years you will be millionaire
+
 x= float(input('original income:'))
 y= float(input('% rate'))  
 c= float(x+(x*y/100))
@@ -6,4 +8,4 @@ while c<1000000 :
     i=i+1
     c= x+c
     
-print('years:',i)
+print('years to wait until you will become millionaire:',i)
